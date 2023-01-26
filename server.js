aa67dc3b3a8242ba1640da651f153b4c72b0294a
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2023-01-26 18:19:15 +0530

    File renamed with a random name

--- server.js
+++ server.js
@@ -3,21 +3,42 @@ const app = express();
 const port = 3000;
 
 const multer = require('multer');
-const upload = multer({dest:'uploads/'}).single("upload_image");
+const path = require('path');
+
+// Generate random string
+const uuid = require("uuid");
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+
+        // Get the uploaded file extension
+        const extname = path.extname(file.originalname).toLowerCase();
+
+        // Rename uploaded file name
+        const newFileName = uuid.v4() + extname;
+
+        // Assign the new file name to the uploaded file so that it can be used later to store in database
+        file.newFileName = newFileName;
+
+        cb(null, file.newFileName);
+    }
+});
+
+const upload = multer({ storage: storage }).single("upload_image");
 
 
-app.post("/image", (req, res) => {
+app.post("/upload-file", (req, res) => {
+
     upload(req, res, (err) => {
-     if(err) {
-       res.status(400).send("Something went wrong!");
-     }
-
-     console.log(req.file);
-
-
-     res.send(req.file);
-   });
- });
+        if (err) {
+            res.status(400).send("Something went wrong!");
+        }
+        res.send(req.file);
+    });
+});
 
 app.get('/', (req, res) => {
     res.send('This from server');
