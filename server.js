2f7e96732b7fbf180b55f45483c5ce058676e501
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2023-01-26 17:58:40 +0530

    Multer tedted

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const multer = require('multer');
+const upload = multer({dest:'uploads/'}).single("upload_image");
+
+
+app.post("/image", (req, res) => {
+    upload(req, res, (err) => {
+     if(err) {
+       res.status(400).send("Something went wrong!");
+     }
+
+     console.log(req.file);
+
+
+     res.send(req.file);
+   });
+ });
+
 app.get('/', (req, res) => {
     res.send('This from server');
 });
